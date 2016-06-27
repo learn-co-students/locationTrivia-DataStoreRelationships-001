6668f04166044f123fd32d2ff85a5ad8d100eb60
John Richardson <richardsonjm@gmail.com>
2016-06-27 12:17:10 -0400

    Locations data store class

---
+++ locationTrivia-dataStore/FISLocationsDataStore.h
@@ -0,0 +1,17 @@
+//
+//  FISLocationsDataStore.h
+//  locationTrivia-dataStore
+//
+//  Created by John Richardson on 6/27/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISLocationsDataStore : NSObject
+
+@property (strong, nonatomic) NSMutableArray *locations;
+
+-(instancetype)init;
+
+@end
